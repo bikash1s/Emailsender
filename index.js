e6b39e0f81fab629c4a4fe27e9c1cf733dffe337
Sovik777 <sovikdutta777@gmail.com>
2024-02-21 12:05:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// create the middleware for the parsing requested bodies
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//define to the server that the static files are stored inside the public
+app.use (express.static('public'));
+
+//defining routes
+app.get('/',(req,res)=>
+{
+    res.sendFile(__dirname+'/public/send-email.html');
+});
+
+//configure nodemailer
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+
+    service:'gmail',
+    auth:{
+        user:'sharmabikash0696@gmail.com',
+        pass:'rhma aktv jstm itjv',
+    }
+});
+
+//create the route for the form
+app.post('/send-email',(req,res)=>{
+    const {to,subject,text} =req.body;
+
+    const mailoptions ={
+        to,
+        subject,
+        text
+    };
+
+
+transporter.sendMail(mailoptions,(error,infor)=>
+{
+if(error){
+        console.error(error);
+        res.status(500).send('error in sending mail');
+}else{
+        console.log('email sent:'+infor.response);
+        res.send('email sent successfully');
+}
+});
+
+
+});
+
+app.listen(port,()=>
+{
+    console.log(`server is running on port ${port}`)
+});
